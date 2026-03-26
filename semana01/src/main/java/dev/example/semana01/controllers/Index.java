586153c21322c72ba5ad584df85bd901c8abd87f
TicseTorresArley <u23238849@utp.edu.pe>
2026-03-26 15:43:53 +0000

    Agregar controlador para días de la semana y meses del año

--- semana01/src/main/java/dev/example/semana01/controllers/Index.java
+++ semana01/src/main/java/dev/example/semana01/controllers/Index.java
@@ -4,14 +4,37 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/dias")
 public class Index {
     @GetMapping
     public List<String> diasSemana() {
         return Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+    }
+
+    @GetMapping("/dia")
+    public String diaSemana(@RequestParam int id) {
+        return switch (id) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miércoles";
+            case 4 -> "Jueves";
+            case 5 -> "Viernes";
+            case 6 -> "Sábado";
+            case 7 -> "Domingo";
+            default -> "Número de día no válido. Debe ser entre 1 y 7.";
+        };
+    }
+
+    @GetMapping("/mes/{mes}")
+    public String mesAnio(@PathVariable int mes) {
+        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        return meses[--mes];    
     }
 }
